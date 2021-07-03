141f0ef28aa94d795fd6865b3a985f2fcb4dda13
EP <kjuioqq8@gmail.com>
2021-07-03 12:25:18 +0900

    webhook update complete

--- src/main/java/com/ep/slackbot/scheduler/WeatherScheduler.java
+++ src/main/java/com/ep/slackbot/scheduler/WeatherScheduler.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.net.UnknownHostException;
 
 @Component
@@ -18,6 +19,12 @@ public class WeatherScheduler {
     @Scheduled(cron = "0 0 0/4 ? * MON-FRI", zone = "Asia/Seoul")
     public void weatherMessages() throws UnknownHostException {
         log.info("[TRY] send daily weather message");
+        weatherService.dailyWeather();
+    }
+
+    @PostConstruct
+    public void initMessage() throws UnknownHostException {
+        log.info("[INIT] send daily weather message");
         weatherService.dailyWeather();
     }
 
